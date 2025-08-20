aa538595908ed0fc13f1ccee62bf2f34ba8821a1
aidenmel <143645746+aidenmel@users.noreply.github.com>
2025-08-20 15:27:57 +0000

    Interactive Header Update

--- scripts/header-cursor.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.querySelector("#header_cursor");
-const hoverobject = document.getElementById("#header_hover_object");
-
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = (e.x - 68/2) + 'px';
-    cursor.style.top = (e.y - 68/2) + 'px';
-});
