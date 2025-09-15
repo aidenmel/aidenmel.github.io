86fc866aa0882483ec37e3b9c596b9e3535acca6
aidenmel <143645746+aidenmel@users.noreply.github.com>
2025-09-15 15:28:10 +0000

    New Homepage v0.22

--- scripts/modules/effects.js
+++ scripts/modules/effects.js
@@ -9,29 +9,51 @@ var Root = document.documentElement;
     }
 var Buffer = 16; // Adds some wiggle room
 
+// Numbers
+var currentPallete = 0;
+
+// Tables 
+var scrollPalletes = {
+    0: {
+        '--primary': '#f8f8f8',
+        '--secondary': '#ffffff',
+        '--black': '#252525',
+    },
+};
+
 // Functions
 function addEffect($object, effect, data){
-
-
+    
+    
     if (effect === 'color-shift'){ // When in frame, shift the site's color pallete completely
         var objectStart = $object.offset().top;
         
-        document.addEventListener('scroll', (e) => {
-            if (window.scrollY >= (objectStart - (window.innerHeight / 3) - Buffer) && window.scrollY < $object.height() - Buffer - (window.innerHeight / 3)){
-                console.log($object)
-                $(":root").css({
-                    '--primary': data['primary'],
-                    '--secondary': data['secondary'],
-                    '--black': data['black'],
-                })
-            } else {
-                $(":root").css({
-                    '--primary': PreviousPallete['--primary'],
-                    '--secondary': PreviousPallete['--secondary'],
-                    '--black': PreviousPallete['--black'],
-                })
-            }
+        scrollPalletes[objectStart] = {
+            '--primary': data['primary'],
+            '--secondary': data['secondary'],
+            '--black': data['black']
+        }   
+    }
+}
+
+// Events
+document.addEventListener('scroll', () => {
+    var setPosition;
+
+    for (let position in scrollPalletes){
+        if (window.scrollY >= position){
+            setPosition = position;
+        }
+    }
+
+    if (setPosition !== currentPallete){ // Makes sure that the root is not set already
+        $(":root").css({
+        '--primary': scrollPalletes[setPosition]['--primary'],
+        '--secondary': scrollPalletes[setPosition]['--secondary'],
+        '--black': scrollPalletes[setPosition]['--black'],
         })
     }
 
-}
+    currentPallete = setPosition;
+    console.log(setPosition, currentPallete, scrollPalletes)
+})
