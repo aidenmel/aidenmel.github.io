71a76298e46a6c7c6b7339fe624b9649133cf4ad
aidenmel <143645746+aidenmel@users.noreply.github.com>
2025-10-01 15:27:42 +0000

    Photo Library v0.3

--- asd-projects/image-library/gallery.js
+++ asd-projects/image-library/gallery.js
@@ -14,26 +14,33 @@
     }
     var CURRENT_INDEX = 1;
     var VIEWPORT_WIDTH = $(window).width();
+    var HIDDEN_MODE = false;
 
     // Objects
     var $gallery = $("#gallery");
     var $quickGallery = $("#scroll-library");
-    
+
+    // Tables
+    var DaysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursady', 'Friday', 'Saturday']
+    var listData = {};
+
 // Core Functions
 function createImage(src_link){
     var i;
 
+// Create objects
+
     // Create new list
-    var newItem = $("<li>").appendTo($quickGallery)
+    var $newItem = $("<li>").appendTo($quickGallery)
     
     // Create mini image
     var $thumb = $("<img>").attr('src', src_link)
-        .appendTo(newItem);
+        .appendTo($newItem);
 
     // Add to actual gallery
-    var newImage = $("<li>").appendTo($gallery)
+    var $newImage = $("<li>").appendTo($gallery)
     $("<img>").attr('src', src_link)
-        .appendTo(newImage)
+        .appendTo($newImage)
 
     i = $('#gallery li').length;
 
@@ -41,6 +48,42 @@ function createImage(src_link){
     $thumb.on('click', function(){
         indexImage(i)
     })
+
+    $newImage.on('click', function(){
+        if (HIDDEN_MODE === true){
+            $("#top-bar").removeClass('hide');
+            $("#actions").removeClass('hide');
+        } else {
+            $("#top-bar").addClass('hide');
+            $("#actions").addClass('hide');
+        }
+        HIDDEN_MODE = !HIDDEN_MODE
+    })
+
+// Get data
+
+    listData[i] = {
+        name: '',
+        dayModified: '...',
+        timeModified: '...',
+    }
+
+    var XHR = new XMLHttpRequest(); // starts a XML request
+    XHR.open('HEAD', src_link, true); // XML setup
+    XHR.onreadystatechange = function(){
+        if (XHR.readyState === 4 && XHR.status === 200){ // If the image is loaded & data is ready to be collected
+            var lastModified = XHR.getResponseHeader('Last-Modified')
+            if (lastModified){
+                var date = new Date(lastModified) // Helps with formatting the date
+                listData[i].dayModified = DaysOfWeek[date.getDay()];
+                listData[i].timeModified = date.getHours() + ':' + date.getMinutes() + ' ' + (date.getHours() < 12 ? 'AM' : 'PM');
+            } else {
+                listData[i].dayModified = 'unknown';
+                listData[i].timeModified = 'unknown';
+            }
+        }
+    };  
+    XHR.send(); // Sends request
 
 }
 
@@ -65,6 +108,11 @@ function indexImage(index, dont_animate){
     !(dont_animate) ? (200 * Math.abs(CURRENT_INDEX - index)) : 0
     )
 
+    // Update details
+    console.log(listData)
+    $("#date").text(listData[index - 1].dayModified);
+    $("#time").text(listData[index - 1].timeModified)
+
     // Update index
     CURRENT_INDEX = index
 }
@@ -83,3 +131,19 @@ for (let CATEGORY in IMAGE_SRC){
         createImage('/assets/'+ CATEGORY.toString() +'/'+ IMAGE_SRC[CATEGORY][SRC])
     } 
 }
+
+// Loop through quick gallery to make scale effect
+setInterval(() => {
+    $("#scroll-library").children().each(function(){
+        // Calculate distance from center
+        var distanceFromCenter = Math.abs(($(this).offset().left + $(this).width()/2) - (VIEWPORT_WIDTH/2))
+
+        if (distanceFromCenter < 100){
+            $(this).height((48 + ((1 - (distanceFromCenter/200)) * 8)))
+        } else {
+            $(this).height(48)
+        }
+    })
+}, 50);
+
+indexImage(CURRENT_INDEX, true) // Start with current index
